00f043f120045133d3cc87e299c4993a37d08482
Renan Philipe Marques do Nascimento <renanphilipe@hotmail.com>
2024-12-07 22:45:15 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function  Home() {
+    return <h1>Este é o início de um projeto web!
+        Eu, carinhosamente, escolhi você, amigo, irmão e companheiro, para me acompanhar nessa jornada!
+        Acredite, você faz parte da minha vida e suas opiniões são importantíssimas e impactam no meu ser.
+        Te Amo Matheus!!!❤️ </h1>
+}
+
+export default Home;
