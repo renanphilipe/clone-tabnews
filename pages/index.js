53a6d0c7e3fa4072f7284871230701dac08798bf
Renan Philipe Marques do Nascimento <renanphilipe@hotmail.com>
2024-12-09 17:43:19 +0000

    projeto disponibilizado no servidor da vercel

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ function  Home() {
     return <h1>Este é o início de um projeto web!
         Eu, carinhosamente, escolhi você, amigo, irmão e companheiro, para me acompanhar nessa jornada!
         Acredite, você faz parte da minha vida e suas opiniões são importantíssimas e impactam no meu ser.
-        Te Amo Matheus!!!❤️ </h1>
+        Te Amo Matheus!!!❤️ 
+        
+        OBS: Mudança nova. Antes o projeto era acessado através do serviço que vinha do meu computador, se eu desligasse ele a url não era mais acessível.
+            Agora o site/app fica levantado em um servidor na internet e fica disponível 24h por dia, 7 dias por semana.!!!</h1>
+        
 }
 
 export default Home;
