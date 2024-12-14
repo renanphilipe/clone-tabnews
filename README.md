b84c0369a85fc28926061aa18c8b008b6d632df3
Renan Philipe Marques do Nascimento <renanphilipe@hotmail.com>
2024-12-14 22:31:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do projeto tabnews do curso.dev para fins de estudo
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-function  Home() {
-    return <h1>Este é o início de um projeto web!
-        Eu, carinhosamente, escolhi você, amigo, irmão e companheiro, para me acompanhar nessa jornada!
-        Acredite, você faz parte da minha vida e suas opiniões são importantíssimas e impactam no meu ser.
-        Te Amo Matheus!!!❤️ 
-        
-        OBS: Mudança nova. Antes o projeto era acessado através do serviço que vinha do meu computador, se eu desligasse ele a url não era mais acessível.
-            Agora o site/app fica levantado em um servidor na internet e fica disponível 24h por dia, 7 dias por semana!!!😎</h1>
-        
+function Home() {
+  return (
+    <h1>
+      Este é o início de um projeto web! Eu, carinhosamente, escolhi você,
+      amigo, irmão e companheiro, para me acompanhar nessa jornada! Acredite,
+      você faz parte da minha vida e suas opiniões são importantíssimas e
+      impactam no meu ser. Te Amo Matheus!!!❤️ OBS: Mudança nova. Antes o
+      projeto era acessado através do serviço que vinha do meu computador, se eu
+      desligasse ele a url não era mais acessível. Agora o site/app fica
+      levantado em um servidor na internet e fica disponível 24h por dia, 7 dias
+      por semana!!!😎
+    </h1>
+  );
 }
 
 export default Home;
